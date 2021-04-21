a0a0c009201ca51546b07ba899fffc68d7a456c6
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-04-21 18:45:43 +0500

    add feature Landing Page.

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+        <Link className="navbar-brand flex-grow-1" to="/">
+          Holiday Finder
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end pr-2" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span class="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter } from 'react-router-dom';
+
 import { client } from './utils/client';
-import HomePage from './HomePage';
+import App from './App';
 
 render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <HomePage />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
